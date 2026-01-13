2a37f4e280771f00660770d497cde57ed0ec9043
borisnorm <borisnorm@gmail.com>
2026-01-13 14:46:54 -0800

    Time: 60 ms (85.06%) | Memory: 173.1 MB (81.84%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,56 +1,89 @@
 class LRUCache {
 public:
-    int cap;
+    struct Node{
+        int key;
+        int val;
+        Node* prev;
+        Node* next;
+        Node(int k, int v): key(k), val(v), prev(nullptr), next(nullptr){}
+    };
 
-    // key , kv_it
-    unordered_map<int, list<pair<int, int>>::iterator> key2it;
-    // key, val
-    list<pair<int, int>> kvl;
+    int cap;
+    Node* head;
+    Node* tail;
+
+    unordered_map<int, Node*> key2node;
+
     LRUCache(int capacity) {
         cap = capacity;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head->next = tail;
+        tail->prev = head;    
     }
     
     int get(int key) {
-        if (!key2it.count(key))
+        if (!key2node.count(key))
           return -1;
-        
-        int val = key2it[key]->second;
 
-        updateLRUentry(key, val);
+        Node* node = key2node[key];
+        int val = node->val;
+        updateLRUentry(node);
         return val;
     }
     
     void put(int key, int value) {
-        if (!key2it.count(key) && kvl.size() == cap)
-          removeLRUentry();
+         if (!key2node.count(key) && key2node.size() == cap)
+           removeLRUentry();
         
-        updateLRUentry(key, value);
+         if (key2node.count(key))
+         {
+            Node* node = key2node[key];
+            node->val = value;
+            updateLRUentry(node);
+         }
+         else
+         {
+            Node* node = new Node(key, value);
+            key2node[key] = node;
+            add2Head(node);
+         }
     }
 
-    void updateLRUentry(int key, int val)
+    void add2Head(Node* node)
     {
-        //delete if exising from kvl
-        if (key2it.count(key))
-          kvl.erase(key2it[key]);
+        Node* nxt = head->next;
 
-        //add it to kvl front
-        kvl.push_front({key, val});
+        head->next = node;
+        node->prev = head;
 
-        //update key2it with new it;
-        key2it[key] = kvl.begin();
+        node->next = nxt;
+        nxt->prev = node;
     }
-    
+    void removeNode(Node* node)
+    {
+        Node* prev = node->prev;
+        Node* nxt = node->next;
+
+        prev->next = nxt;
+        nxt->prev = prev;
+
+    }
+    void updateLRUentry(Node* node)
+    {
+        removeNode(node);
+        add2Head(node);
+    }
+
     void removeLRUentry()
     {
-       //get last key first
-       auto it = prev(kvl.end());
-
-       //delete the key2it entry
-       int key = it->first;
-       key2it.erase(key);
-
-       kvl.pop_back();
+        Node* node = tail->prev;
+        removeNode(node);
+        key2node.erase(node->key);
+        delete node;
     }
+    
 };
 
 /**
