34046a57cb9f3749bf92e2cf54625de8bfe217b2
borisnorm <borisnorm@gmail.com>
2026-01-14 09:57:55 -0800

    Time: 0 ms (100.00%) | Memory: 14.6 MB (62.69%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        vector<pair<int, int>> num_vec;
+        for (int i = 0; i < n; i++)
+        {
+           num_vec.push_back({nums[i], i});
+        }
+
+        sort(num_vec.begin(), num_vec.end());
+
+        int l = 0;
+        int r = n - 1;
+
+        vector<int> res;
+        while (l < r)
+        {
+            long long sum = num_vec[l].first + num_vec[r].first;
+            if (sum < target)
+              l++;
+            else if (sum > target)
+              r--;
+            else
+            {
+               int l_idx = num_vec[l].second;
+               int r_idx = num_vec[r].second;
+               if (l_idx > r_idx)
+                 swap(l_idx, r_idx);
+                
+                res.push_back(l_idx);
+                res.push_back(r_idx);
+                return res;
+            }
+        }
+
+        return res;
+    }
+/*
+    vector<int> twoSum(vector<int>& nums, int target) {
         
         unordered_map<int, int> num2idx;
         int n = nums.size();
@@ -22,4 +60,5 @@ public:
 
         return res;
     }
+    */
 };
