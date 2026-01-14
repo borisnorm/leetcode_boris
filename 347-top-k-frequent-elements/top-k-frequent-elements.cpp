e2f48bda3826d2d116d7865301994467a21e6132
borisnorm <borisnorm@gmail.com>
2026-01-14 15:10:16 -0800

    Time: 3 ms (50.97%) | Memory: 20.1 MB (11.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+          // count freq
+         unordered_map<int, int> num2freq;
+         for (int num: nums)
+           num2freq[num]++;
+ 
+         int n = nums.size();
+         vector<vector<int>> bucket(n+1);
+         // insert freq into buckets
+         for (auto it: num2freq)
+           bucket[it.second].push_back(it.first);
+        
+         // traverse the buckets in reverse order to fill the result
+         int cnt = 0; 
+         vector<int> res;
+         for (int i = n; i >= 1 && cnt < k; i--)
+         {
+            auto val_vec = bucket[i];
+            for (int num: val_vec)
+            {
+                res.push_back(num);
+                cnt++;
+                if (cnt == k)
+                  break;
+            }
+         }
+
+         return res;
+    }
+   /*
+    vector<int> topKFrequent(vector<int>& nums, int k) {
          int n = nums.size();
 
          unordered_map<int, int> num2freq;
@@ -27,4 +57,6 @@ public:
          }
          return res;
     }
+
+    */
 };
