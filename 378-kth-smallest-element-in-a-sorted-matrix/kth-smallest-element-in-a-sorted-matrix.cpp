18c6004cc01e9eea85975eab05251d2bcdf1c582
borisnorm <borisnorm@gmail.com>
2026-01-14 15:25:14 -0800

    Time: 3 ms (66.02%) | Memory: 17.1 MB (66.07%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) { 
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
         int n = matrix.size();
-
         int l = matrix[0][0];
         int r = matrix[n-1][n-1];
 
-        // find the first >= k value lower_bound
-        while(l < r)
+        // found first >= K 
+        while (l < r)
         {
-            long long mid = l + (r - l)/2;
+           long long mid = 1LL* l + (1LL* r - l) / 2;
 
-            if (countLessEqual(matrix, mid) < k)
-              l = mid + 1;
-            else
-              r = mid;
-        }
-
-        return l;
-        
-    }
-
-    int countLessEqual(vector<vector<int>>& matrix, int target)
-    {
-        int n = matrix.size();
-        int r = 0;
-        int c = n - 1;
-        
-        int cnt = 0;
-        while (r < n && c >=0)
-        {
-           if (matrix[r][c] <= target)
-           {
-              cnt += (c + 1);
-              r++;
-           }
+           if (countLessEqual(matrix, mid) < k)
+             l = mid + 1;
            else
-           {
-              c--;
-           }
+             r = mid;
         }
-
-        return cnt;
+        return l;
     }
+
+     int countLessEqual(vector<vector<int>>& matrix, int target)
+     {
+         int n = matrix.size();
+         
+         int r = 0;
+         int c = n-1;
+         int cnt = 0;
+         while (r < n && c >=0)
+         {
+            if (matrix[r][c] <= target)
+            {
+                cnt+= (c + 1);
+                r++;
+            }
+            else
+            {
+                c--;
+            }
+         }
+
+         return cnt;
+     }
 };
