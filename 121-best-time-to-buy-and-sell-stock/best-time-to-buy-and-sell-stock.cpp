82d7dd95018b56877fc029bb682dc80c1779b527
borisnorm <borisnorm@gmail.com>
2026-01-15 18:11:26 -0800

    Time: 0 ms (100.00%) | Memory: 97.5 MB (24.61%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+   
+    // 1 <= prices.length <= 10^5
+    // 0 < price[i] <= 10^4
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        if (n < 2)
+          return 0;
+        
+        int minPrice = INT_MAX;
+        int profit = 0;
+        int maxProfit = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+           minPrice = min(prices[i], minPrice);
+
+           if (prices[i] > minPrice)
+           {
+             profit = prices[i] - minPrice;
+             maxProfit = max(maxProfit, profit);
+           }
+        }
+
+        return maxProfit;
+    }
+};
